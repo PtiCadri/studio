64a7d2037503884819868d53ce9dc5d916580cf3
mdepreau <martindeppro@gmail.com>
2026-02-18 13:13:25 +0100

    second commit - arborescence setup serveur

---
+++ backend/internal/handler/handlers.go
@@ -0,0 +1,6 @@
+package handler
+
+func handle() {
+	// This is where you can set up your HTTP handlers and start your server.
+	// For example, you might want to use the net/http package to create routes and listen on a port.
+}
