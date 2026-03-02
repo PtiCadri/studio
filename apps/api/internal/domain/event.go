7f04d69e3b4d2d59bcaa386e67ad1ee42e2f475c
PtiCadri <PtiCadri@github.com>
2026-03-02 16:05:26 +0100

    adding commentary

--- apps/api/internal/domain/event.go
+++ apps/api/internal/domain/event.go
@@ -6,6 +6,7 @@ import (
 	"github.com/google/uuid"
 )
 
+// Event represents a musical event with relevant information.
 type Event struct {
 	ID          uuid.UUID  `json:"id"`
 	Name        string     `json:"name"`
@@ -16,6 +17,7 @@ type Event struct {
 	CreatedAt   time.Time  `json:"created_at"`
 }
 
+// UpdateEventInput is used for updating an event's information.
 type UpdateEventInput struct {
 	Name        string     `json:"name"`
 	Description string     `json:"description"`
