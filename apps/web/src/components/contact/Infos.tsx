15474574d6b759a37b408671037de247f2e6b6cf
LilGreyCat <tanguy-defrance@outlook.com>
2026-03-25 21:12:20 +0100

    fix: add padding to Typography components and adjust box position for improved layout

--- apps/web/src/components/contact/Infos.tsx
+++ apps/web/src/components/contact/Infos.tsx
@@ -5,19 +5,19 @@ export default function Infos() {
 
     return (
         <Box sx={boxSx}>
-            <Typography variant="body1" gutterBottom>
+            <Typography variant="body1" sx={{ pl: "10px" }} gutterBottom>
                 <strong>Studio Nha Dès Records</strong>
             </Typography>
 
-            <Typography variant="body1" gutterBottom>
+            <Typography variant="body1" sx={{ pl: "10px" }} gutterBottom>
                 nhadesrecords@outlook.fr
             </Typography>
 
-            <Typography variant="body1" gutterBottom>
+            <Typography variant="body1" sx={{ pl: "10px" }} gutterBottom>
                 06.50.46.24.88
             </Typography>
 
-            <Typography variant="body1" gutterBottom>
+            <Typography variant="body1" sx={{ pl: "10px" }} gutterBottom>
                 47 Rue des Canadiens<br/>76420 - Bihorel
             </Typography>
 
@@ -28,7 +28,7 @@ export default function Infos() {
 
 const boxSx = {
     position: "absolute",
-    top: "280px",
+    top: "320px",
     left: "10px",
     color: "text.secondary",
     display: "flex",
