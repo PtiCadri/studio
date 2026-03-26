cb9840673123bab13ad86a88ae1e2b27cbfd63c7
LilGreyCat <tanguy-defrance@outlook.com>
2026-03-26 17:52:29 +0100

    feat: add contact button to HomePage for improved navigation

--- apps/web/src/app/(pages)/(home)/page.tsx
+++ apps/web/src/app/(pages)/(home)/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { Box } from "@mui/material";
+import Link from "next/link";
+import { Box, Button } from "@mui/material";
 import { Prestations } from "@/components/home/prestations";
 import PresentationStudio from "@/components/home/PresentationStudio";
 import Carousel from "@/components/ui/Carousel";
@@ -20,8 +21,20 @@ export default function HomePage() {
       <Prestations />
       <Carousel />
       <PresentationStudio />
+      <Button
+        variant="outlined"
+        component={Link}
+        href="/contact"
+        aria-label="Contactez le studio"
+        sx={btnSx}
+      >
+        Contactez le studio
+      </Button>
     </>
   );
 }
 
-
+const btnSx = {
+  fontSize: "24px",
+  borderColor: "text.primary",
+};
