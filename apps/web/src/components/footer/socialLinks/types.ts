09d654c20a9cb16eef9c254615458f2cb259e688
LilGreyCat <tanguy-defrance@outlook.com>
2026-03-30 11:42:44 +0200

    feat: define CustomIconInfo and CustomIconKey types; update iconPaths and customIcons structure

---
+++ apps/web/src/components/footer/socialLinks/types.ts
@@ -0,0 +1,15 @@
+type CustomIconInfo = {
+    path: string;
+    link: string;
+};
+
+type CustomIconKey =
+    | "instagram"
+    | "spotify"
+    | "appleMusic"
+    | "deezer"
+    | "youtube"
+    | "tiktok"
+    | "facebook";
+
+export type { CustomIconInfo, CustomIconKey };
